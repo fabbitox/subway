f022cff601feffde4ca275efd8dafacdc71d29f9
fabbitox <hyunjin_11@naver.com>
2023-07-25 16:17:24 +0900

    start

---
+++ src/main/Select.js
@@ -0,0 +1,12 @@
+import style from './Select.module.css';
+
+const Select = () => {
+    return (
+        <main>
+            <header><h1>부산 지하철 정보</h1></header>
+            <main>역을 선택하세요</main>
+        </main>
+    );
+}
+
+export default Select;
