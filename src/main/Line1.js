99a86c5f461a05f3f7d4980a2bad7eb08c32521a
fabbitox <hyunjin_11@naver.com>
2023-07-27 18:01:38 +0900

    0727

--- src/main/Line1.js
+++ src/main/Line1.js
@@ -9,8 +9,7 @@ const Line1 = () => {
     const buttonrow = <div className={style.row}>{buttons}</div>;
 
     return (
-        <div>
-            <img src={'img/1호선 노선도(Line 1 Map).png'} alt='1호선 노선도' className={style.line1}></img>
+        <div className={style.map}>
             {buttonrow}
         </div>
     );
