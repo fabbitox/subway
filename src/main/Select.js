7c88545bf90f9b531affd428828ce7d831902b63
fabbitox <hyunjin_11@naver.com>
2023-07-28 17:57:08 +0900

    0728

--- src/main/Select.js
+++ src/main/Select.js
@@ -1,5 +1,5 @@
 import style from './Select.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Line1 from './Line1';
 import Line2 from './Line2';
 import Line3 from './Line3';
@@ -26,23 +26,38 @@ const Select = () => {
     const classnames = [style.line1, style.line2, style.line3, style.line4];
     const [line, setline] = useState(-1);
     const lines = ['1호선', '2호선', '3호선', '4호선'];
-    const btntags = lines.map((item, idx) =>
+    const linebtns = lines.map((item, idx) =>
         <button onClick={() => setline(idx)}
         className={classnames[idx] + ' ' + style.large + (idx === line ? ' ' + style.selected : '')}
         key={lines[idx]}>
             {item}
         </button>
     );
+    const [endidx, setend] = useState(-1);
+    const end = [['노포:134', '신평:101', '다대포해수욕장:095'], ['장산:201', '광안:209', '전포:219', '호포:239', '양산:243'],
+        ['수영:301', '대저:317'], ['안평:414', '미남:401']];
+    const endbtns = end.map((lineend, lineidx) => lineend.map(
+        (item, idx) => <button key={item} onClick={() => setend(idx)}
+            className={style.end + ' ' + classnames[lineidx] + ' ' + style.normal + (idx === endidx ? ' ' + style.selected : '')}>
+            {item.split(':')[0]}
+        </button>)
+    );
     const linemap = [<Line1></Line1>, <Line2></Line2>, <Line3></Line3>, <Line4></Line4>];
     const totalmap = <img src='img/종합노선도(Metro Line Map).png' alt='종합노선도'></img>
-    const map = line === -1 ? totalmap : linemap[line];
+
+    useEffect(() => {
+        setend(-1);
+    }, [line]);
 
     return (
         <main>
             <header><h1 className={style.center}>부산 지하철 정보</h1></header>
-            <div><strong className={style.left + ' ' + style.xlarge}>역을 선택하세요</strong>{btntags}</div>
-            <div className={style.up}><strong className={style.large + ' ' + style.left}>{getDayType(time.getDay())} {timeformat(time)}</strong></div>
-            {map}
+            <div><strong className={style.left + ' ' + style.xlarge}>출발역과 방향을 선택하세요</strong>{linebtns}</div>
+            <div className={style.up}>
+                <strong className={style.large + ' ' + style.left}>{getDayType(time.getDay())} {timeformat(time)}</strong>
+                {line !== -1 && endbtns[line]}
+            </div>
+            {line === -1 ? totalmap : endidx !== -1 && linemap[line]}
         </main>
     );
 }
