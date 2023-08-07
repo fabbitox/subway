93354ee1d82fa4f9c8ff5161458c8f7a4818e6f5
fabbitox <hyunjin_11@naver.com>
2023-08-07 12:22:42 +0900

    login

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import style from './Main.module.css'
+import styled from 'styled-components'
 import Select from './Select'
 import { Link } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
+import { useState, useEffect } from 'react'
+
+const Button = styled.button`
+    padding: 0.15rem 0.3rem;
+    border-radius: 0.5rem;
+    border: 0.12vmax solid #00000099;
+    background: radial-gradient(white, #ff3366 50%);
+    &:hover {
+        background: radial-gradient(white, #ff3366 75%);
+        border: 0.15vmax solid black;
+        font-weight: bold;
+    }
+`;
 
 const Main = () => {
+    const [userid, setUser] = useState(null);
+    const logout = () => {
+        localStorage.removeItem('userid');
+        localStorage.removeItem('accesstoken');
+        setUser(null);
+    };
+    useEffect(() => {
+        setUser(localStorage.getItem('userid'));
+    }, []);
+    const info = <span className={style.info}>{userid}님 <Button onClick={logout}>로그아웃</Button></span>;
     return (
         <main>
             <Helmet><title>부산 지하철 정보</title></Helmet>
             <header><h1 className={style.center}>부산 지하철 정보</h1></header>
-            <Link to='/login' className={style.login}>로그인 / 회원가입</Link>
+            {userid == null ? <Link to='/login' className={style.login}>로그인 / 회원가입</Link> : info}
             <Select></Select>
         </main>
     )
--- src/user/LoginJoin.js
+++ src/user/LoginJoin.js
@@ -4,6 +4,8 @@ import userst from './User.module.css';
 import style from './LoginJoin.module.css';
 import styled from 'styled-components';
 import { Helmet } from 'react-helmet';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Button = styled.button`
     width: 8rem;
@@ -24,31 +26,46 @@ const Button = styled.button`
 const LoginJoin = () => {
     const [userId, setUserId] = useState('');
     const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    const goHome = () => navigate('/');
     const login = () => {
-        console.log('로그인', userId, password);
+        const member = { username: userId, password: password };
+        axios.post('http://10.125.121.185:8080/login', member).then((response) => {
+            if (response.data.message === '로그인 성공') {
+                console.log(response.headers.getAuthorization());
+                localStorage.setItem('userid', userId);
+                localStorage.setItem('accesstoken', response.headers.getAuthorization());
+                goHome();
+            }
+        }).catch(() => {
+            console.log('로그인 실패');
+            localStorage.removeItem('userid');
+            localStorage.removeItem('accesstoken');
+        });
     }
     const register = () => {
-        console.log('회원가입', userId, password);
+        const member = { username: userId, password: password };
+        axios.post('http://10.125.121.185:8080/register', member).then((response) => {
+            console.log(response.data);
+        }).catch(() => console.log('회원가입 실패'));
     }
 
     return (
-        <>
+        <div className={userst.center} style={{border: '0.12vmax solid #00000099', borderRadius: '0.5rem', marginTop: '-10%'}}>
             <Helmet><title>부산 지하철 정보 | 로그인</title></Helmet>
-            <div className={userst.center} style={{border: '0.12vmax solid #00000099', borderRadius: '0.5rem', marginTop: '-10%'}}>
-                <div className={style.input}>
-                    <label htmlFor="userId" className={style.lj}>아이디</label>
-                    <input type="text" id="userId" value={userId} onChange={e => setUserId(e.target.value)} className={style.lj} />
-                </div>
-                <div className={style.input}>
-                    <label htmlFor="password" className={style.lj}>비밀번호</label>
-                    <input type="password" id="password" value={password} onChange={e => setPassword(e.target.value)} className={style.lj} />
-                </div>
-                <div className={style.flex}>
-                    <Button onClick={login} className={style.lj} $color='#9966ff'>로그인</Button>
-                    <Button onClick={register} className={style.lj} $color='#33ff66'>회원가입</Button>
-                </div>
+            <div className={style.input}>
+                <label htmlFor="userId" className={style.lj}>아이디</label>
+                <input type="text" id="userId" value={userId} onChange={e => setUserId(e.target.value)} className={style.lj} />
             </div>
-        </>
+            <div className={style.input}>
+                <label htmlFor="password" className={style.lj}>비밀번호</label>
+                <input type="password" id="password" value={password} onChange={e => setPassword(e.target.value)} className={style.lj} />
+            </div>
+            <div className={style.flex}>
+                <Button onClick={login} className={style.lj} $color='#9966ff'>로그인</Button>
+                <Button onClick={register} className={style.lj} $color='#33ff66'>회원가입</Button>
+            </div>
+        </div>
     )
 }
 
